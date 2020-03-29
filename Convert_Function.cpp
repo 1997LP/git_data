cacb7729066bebc255b02c2f13aab929490a386f
1997LP <1950517655@qq.com>
2020-03-29 21:10:37 +0800

    string

---
+++ Convert_Function.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+//#include <iostream>
+//
+//
+//int my_atoi(const char *str)
+//{
+//	int ret = 0;
+//	int i = 0;
+//	if (str == NULL)return 0;
+//	else
+//	{
+//		ret = str[0] - '0';
+//		while (*(str+i+1)!='\0')
+//		{
+//			i++;
+//		}
+//		ret *= pow(10,i);
+//		return ret + my_atoi(str+1);
+//	}
+//}
+//
+//
+//int main()
+//{
+//	printf("%d", my_atoi("12"));
+//}
